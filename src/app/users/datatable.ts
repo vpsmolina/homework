f7ec9ca52e4519093217a07aa48b04484ccd20e9
vpsmolina <verasmolinap@mail.ru>
2019-12-15 23:30:47 +0300

    Initial commit

--- src/app/users/datatable.ts
+++ src/app/users/datatable.ts
@@ -5,13 +5,14 @@ import { User } from "./user";
 @Injectable({providedIn: "root"})
 export class DataTable {
   public users: User[] = [
-    {id: 1, name: "Иван", surname: "Иванов", middlename: "Александрович", birthday: new Date("1994-08-16"), coefficient: 2},
+    {id: 1, name: "Иван", surname: "Иванов", middlename: "Александрович", birthday: new Date("1994-08-16"), coefficient: 5},
     {id: 2, name: "Петр", surname: "Петров", middlename: "Алексеевич", birthday: new Date("1995-07-24"), coefficient: 4},
-    {id: 3, name: "Алексей", surname: "Алексеев", middlename: "Андреевич", birthday: new Date("1996-04-21"), coefficient: 5},
+    {id: 3, name: "Алексей", surname: "Алексеев", middlename: "Андреевич", birthday: new Date("1996-04-21"), coefficient: 2},
   ];
 
   removeUser( id: number): void {
     this.users = this.users.filter(t => t.id !== id);
   }
+
 }
 
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -12,29 +12,56 @@ import { User } from "./user";
 })
 export class UsersComponent implements OnInit {
   private searchName: string = "";
-  nameControl: FormControl;
   fullnameControl: FormGroup;
+  fullname: FormGroup;
   constructor(private dataTable: DataTable) {
   }
   ngOnInit(): void {
-    this.nameControl = new FormControl("Vera");
-    this.nameControl.statusChanges.subscribe((status) => {
-      console.log(this.nameControl.errors);
-      console.log(status);
-    });
+    this.initForm();
+  }
+  initForm(): void {
     this.fullnameControl = new FormGroup({
-      name: new FormControl(),
-      surname: new FormControl(),
-      middlename: new FormControl(),
-      birthday: new FormControl(),
-      coefficient: new FormControl()
+      fullname: new FormGroup({
+        name: new FormControl("", [Validators.required]),
+        surname: new FormControl("", [Validators.required]),
+        middlename: new FormControl("", [Validators.required]),
+      }/*, {validators: nameValidator}*/),
+      birthday: new FormControl("", [Validators.required, dateValidator]),
+      coefficient: new FormControl("", [Validators.required]),
     });
   }
-
-
+  onSubmit(): void {
+    const controls = this.fullnameControl.controls;
+    if (this.fullnameControl.invalid) {
+      Object.keys(controls)
+        .forEach(controlName => controls[controlName].markAsTouched());
+      return;
+    }
+    console.log(this.fullnameControl.get("fullname.name").value);
+    this.dataTable.users.push(this.fullnameControl.value);
+  }
   removeUser(id: number): void {
     this.dataTable.removeUser(id);
   }
+}
+// tslint:disable-next-line:typedef
+function dateValidator (formControl: FormControl) {
+  const nday = new Date;
+  const bday = formControl.value.split("-")[0];
+  if ( bday > (nday.getFullYear() - 10) ) {
+    return {dateValidator: {message: "Not found"}};
+  }
+  return null;
+}
+// tslint:disable-next-line:typedef
+function nameValidator(control: FormGroup) {
+  const firstname = control.get("name").value;
+  const lastname = control.get("surname").value;
+  const middlename = control.get("middlename").value;
+  if (firstname.length > 3) {
+    return {nameValidator: {message: "Not found"}};
+  }
+  return null;
 }
 
 
