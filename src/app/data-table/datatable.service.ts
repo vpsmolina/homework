85637d5518cb81b57c798a26dfcae354b049a9f6
vpsmolina <verasmolinap@mail.ru>
2020-01-28 09:12:35 +0300

    Deleted comments.

--- src/app/data-table/datatable.service.ts
+++ src/app/data-table/datatable.service.ts
@@ -12,32 +12,23 @@ import { UsersList } from "./userslist";
 export class DataTableService implements UserData {
   private _users: User[] = UsersList;
   private _user: User;
-  /*constructor(private noticeService: NoticeService) {}*/
+
 
   public getUsers(): Observable<User[]> {
-    return of(this._users)/*.pipe(
-      catchError(this.noticeService.handleError<User[]>("Error")),
-    )*/;
+    return of(this._users);
   }
   public deleteUser(_id: string): Observable<User> {
     this._users = this._users.filter(user => user._id !== _id ? this._user = user : this._user = null);
-    return of(this._user)/*.pipe(
-      tap(() => this.noticeService.add({type: "success", message: "Student succeccfully deleted"})),
-      catchError(this.noticeService.handleError<User>("Error")),
-    )*/;
+    return of(this._user);
   }
   public createUser(data: User): Observable<User> {
     /*this._users.push(data);*/
-    return of(data)/*.pipe(
-      tap(() => this.noticeService.add({type: "success", message: "User successfully added"})),
-      catchError(this.noticeService.handleError<User>("Error")),
-    )*/;
+    return of(data);
   }
   public getUserById(_id: string): Observable<User[]> {
     this._user = this._users.find(user => user._id === _id);
     return of(this._user).pipe(
       toArray(),
-      /*catchError(this.noticeService.handleError<User[]>("Error")),*/
     );
   }
 
